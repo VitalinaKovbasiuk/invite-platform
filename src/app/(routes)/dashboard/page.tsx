f2a1475f93a18d231474823327b909f4ea04e23a
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2024-06-15 20:43:52 +0300

    home page

---
+++ src/app/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+// import FormComponent from "@/components/form-component";
+
+const CreateInvitationPage = () => {
+  return (
+    <div className="container py-[100px]">
+      <h1>Hello i am create invitation PAGE</h1>
+      <div className="border border-primary p-8 rounded-3xl">
+        <h1>Hello</h1 >
+      </div>
+    </div>
+  );
+};
+
+export default CreateInvitationPage;
