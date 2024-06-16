366fbb3337dbacdf2d022b4cf747d6e668da5a94
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2024-06-16 18:04:24 +0300

    create schema, create post and get

---
+++ src/app/api/invites/route.ts
@@ -0,0 +1,31 @@
+
+import { prisma } from "@/lib/prismadb";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+
+    const invite = await prisma.invite.create({
+        data: {
+           ...body,
+            Guests: {
+                create: body.guests,
+            },
+        },
+    });
+
+    return NextResponse.json(invite, { status: 201 });
+}
+
+export async function GET() {
+    const invites = await prisma.invite.findMany({
+        include: {
+            Guests: true,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+    return NextResponse.json(invites);
+}
